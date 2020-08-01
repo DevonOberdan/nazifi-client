49035aa30b976e53355470bbeec3dab938b016f9
Devon Oberdan <devonoberdan@gmail.com>
2020-07-31 23:55:38 -0700

    fixed

---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import { Link } from 'react-router-dom'
+
+
+import AppBar from '@material-ui/core/AppBar'
+
+//import Drawer from '@material-ui/core/Drawer'
+import { Toolbar, Button } from '@material-ui/core'
+
+
+export default function Header(){
+    return (
+        <AppBar>
+            <Toolbar>
+                <Button>
+                    <Link to="/">Home</Link>
+                </Button>
+            </Toolbar>
+        </AppBar>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+import {Router, Switch, Route } from 'react-router'
+
+import 'assets/css/index.css';
+import App from 'views/App';
+import * as serviceWorker from 'components/support/serviceWorker';
+
+
+
+import { createBrowserHistory } from 'history';
+
+var hist = createBrowserHistory()
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <App/>,
+  <Router history={hist}>
+      <Switch>
+        <Route path="/" component={App}/>
+      </Switch>
+    </Router>,
+
   document.getElementById('root')
 );
 
