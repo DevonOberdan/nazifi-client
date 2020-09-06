a5cfe219a71502e95e6ea65e48a9b9e2ed992178
Devon Oberdan <devonoberdan@gmail.com>
2020-09-05 20:04:09 -0700

    Updates

---
+++ precache-manifest.fae536310742cd694054e68775021f19.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1cd03b059769277ef4fcae9fbf9fd0c",
+    "url": "/nazifi-client/index.html"
+  },
+  {
+    "revision": "b46c515f2ec39b3c71ad",
+    "url": "/nazifi-client/static/css/main.344a1351.chunk.css"
+  },
+  {
+    "revision": "065bee673ca0200c68e7",
+    "url": "/nazifi-client/static/js/2.12b0cf3a.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/nazifi-client/static/js/2.12b0cf3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b46c515f2ec39b3c71ad",
+    "url": "/nazifi-client/static/js/main.e7e721ad.chunk.js"
+  },
+  {
+    "revision": "6014280ce0423a9fc67b",
+    "url": "/nazifi-client/static/js/runtime-main.f786b581.js"
+  },
+  {
+    "revision": "ccca136710a5b39694b129491999239f",
+    "url": "/nazifi-client/static/media/Nazifi_cool-dark4x.ccca1367.png"
+  },
+  {
+    "revision": "6c2a20e88ac96cfec59143383d3dcbcb",
+    "url": "/nazifi-client/static/media/Nazifi_cool4x.6c2a20e8.png"
+  },
+  {
+    "revision": "865c3638244dc21b4cf9f7aae552213b",
+    "url": "/nazifi-client/static/media/Nazifi_cool_text4x.865c3638.png"
+  },
+  {
+    "revision": "fbcb3b61f2a252bc5fd65a8a44f87108",
+    "url": "/nazifi-client/static/media/Nazifi_warm4x.fbcb3b61.png"
+  },
+  {
+    "revision": "8eea149c3e74974bf8b8d0949358e262",
+    "url": "/nazifi-client/static/media/Nazifi_warm_text4x.8eea149c.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nazifi-client/static/media/logo.5d5d9eef.svg"
+  }
+]);
